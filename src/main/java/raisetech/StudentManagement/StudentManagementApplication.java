37104900949ffc72883966381c90f43ca7c87e43
Kushima <hi.kuromizu@gmail.com>
2026-07-14 12:11:54 +0900

    lesson23_StudentsCoursesクラスの作成_デーãlLombokの追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,17 +1,10 @@
 package raisetech.StudentManagement;
 
-import ch.qos.logback.core.util.StringUtil;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,33 +20,12 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-  @GetMapping("/student")
-  public String getStudent(@RequestParam String name){
-    Student student = repository.searchByName(name);
-
-    return student.getName() + " " + student.getAge() + "歳";
+  @GetMapping("/studentsList")
+  public List<Student> getStudentList(){
+    return repository.search();
   }
-
-  @PostMapping("/student")
-  public void registerStudent(String name,int age){
-    repository.registerStudent(name,age);
+  @GetMapping("/studentsCoursesList")
+  public List<StudentsCourses> getStudentsCoursesList(){
+    return repository.searchByStudentsCourses();
   }
-
-  @PatchMapping("/student")
-  public void updateStudentAge(String name,int age){
-  repository.updateStudent(name,age);
-  }
-
-  @DeleteMapping("/student")
-  public void deleteStudent(String name){
-    repository.deleteStudent(name);
-  }
-
-  @GetMapping("/studentMap")
-  public Map<String,Integer> createStudentMap(){
-    Map<String,Integer> studentMap = new HashMap<>();
-    repository.getStudentRecord().forEach(student -> studentMap.put(student.getName(),student.getAge()));
-    return studentMap;
-  }
-
 }
